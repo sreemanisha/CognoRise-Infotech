b72fd211d5d99d494439c939c015b197fc8a415d
sreemanisha <113758131+sreemanisha@users.noreply.github.com>
2024-02-06 13:32:51 +0530

    Create CA.java

---
+++ CA.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class CalculatorApp {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Enter second number: ");
+        double num2 = scanner.nextDouble();
+
+        System.out.print("Enter operator (+, -, *, /): ");
+        char operator = scanner.next().charAt(0);
+
+        try {
+            double result;
+            switch (operator) {
+                case '+':
+                    result = num1 + num2;
+                    System.out.println("Result: " + result);
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    System.out.println("Result: " + result);
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    System.out.println("Result: " + result);
+                    break;
+                case '/':
+                    if (num2 == 0) {
+                        throw new ArithmeticException("Division by zero is not allowed.");
+                    }
+                    result = num1 / num2;
+                    System.out.println("Result: " + result);
+                    break;
+                default:
+                    System.out.println("Invalid operator!");
+            }
+        } catch (ArithmeticException e) {
+            System.out.println("Error: " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Error: Invalid input.");
+        } finally {
+            scanner.close();
+        }
+    }
+}
